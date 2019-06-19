dba775054e368c7ac5289b7ccbc4e4f76c94370e
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 15:09:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     puts programmer_hash[:alan_kay][:known_for]
      
   programmer_hash
 end
@@ -132,4 +131,3 @@ def adding_to_dennis
 end
 
 
-changing_alan
